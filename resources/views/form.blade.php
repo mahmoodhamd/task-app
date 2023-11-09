1e61fc7b982f0ff5cf8a651ce8ee53eb7e95cdb9
mahmoodhamd <116299051+mahmoodhamd@users.noreply.github.com>
2023-11-09 16:56:55 +0500

    styling applying in the create and edit view

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,71 +1,86 @@
 @extends('layouts.app')
 @section('title',isset($task)? 'Edit Task':'Add Task')
-    
 
-@section ('styles') 
+
+@section ('styles')
 <style>
 .error-message{
     color:red;
+    font-size: 0.8rem;
 }
 </style>
 @endsection
 
 @section('content')
     <form method="POST" action ="{{ isset($task) ? route('tasks.update',['task'=>$task->id]) : route('tasks.store') }}">
-    
+
      @csrf
      @isset($task)
       @method('PUT')
      @endisset
-    <div>
+
+       <div class="mb-4">
         <label for="title">
             Title
-        </label> 
-        <input class="px-4 py-4" type="text" name="title" id="title" value="{{$task->title ?? old('title')}}" />
+        </label>
+        <input text="text" name="title" id="title"
+        @class(['border-red-500'=>$errors->has('title')])
+        value="{{$task->title ?? old('title')}}" />
         @error('title')
-        <p class="error-message">{{$message}} </p>
+        <p class="error">{{$message}} </p>
         @enderror
     </div>
 
-     <div>
+     <div class="mb-4">
       <label for="description">Description</label>
-      <textarea name="description" id="description" rows="5" >{{$task->description ??old('description')}}</textarea>
+      <textarea name="description" id="description"
+      @class(['border-red-500'=>$errors->has('description')])
+      rows="5" >{{$task->description ??old('description')}}
+
+     </textarea>
       @error('description')
-      <p class="error-message">{{$message}} </p>
+      <p class="error">{{$message}} </p>
       @enderror
      </div>
-      
-     
+
+
      <div>
         <label for="long_description">long Description</label>
-        <textarea name="long_description" id="long_description" rows="5"  > {{$task->long_description ?? old('long_description')}} </textarea>
+        <textarea name="long_description" id="long_description"
+        @class(['border-red-500' =>$errors->has('long_description')])
+         rows="5"> {{$task->long_description ?? old('long_description') }}
+        </textarea>
         @error('long_description')
-        <p class="error-message">{{$message}} </p>
+        <p class="error">{{$message}} </p>
         @enderror
        </div>
-       
-      
+
+
      <div>
         <label for="summary">Summary</label>
-        <textarea name="summary" id="summary" rows="5"  >{{$task->summary ?? old('summary')}}</textarea>
+        <textarea name="summary" id="summary"
+        @class(['border-red-500'=>$errors->has('title')])
+        rows="5"  >{{$task->summary ?? old('summary')}}
+     </textarea>
         @error('summary')
-        <p class="error-message">{{$message}} </p>
+        <p class="error">{{$message}} </p>
         @enderror
        </div>
-     
 
-       
+
+
 
        <div class="mt-4">
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded" type="submit">
                 @isset($task)
                 Update task
                 @else
-                Add task 
-                @endisset 
+                Add task
+                @endisset
         </button>
+        <a href="{{route('tasks.index')}}" class="link">Cancel </a>
        </div>
-  
-     
+
+
     </form>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,30 @@
 <title> Laravvel 10 Task List App</title>
 <script src="https://cdn.tailwindcss.com"></script>
 
+
 @yield('styles')
+<style type="text/tailwindcss">
+.link {
+    @apply font-medium text-gray-700 underline decoration-pink-500
+  }
+  label {
+    @apply block uppercase text-slate-700 mb-2
+  }
+  input,
+  textarea {
+    @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+  }
+  .error {
+    @apply text-red-500 text-sm
+  }
+</style>
+
+
 </head>
 <body class="continer mx-auto mt-10 mb-10 max-w-lg">
     <h1 class="text-2xl mb-4">@yield('title') </h1>
     <div>
-     
+
         @if(session()->has('success'))
       <div>  {{session('success')}}</div>
         @endif
